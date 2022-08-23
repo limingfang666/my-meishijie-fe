d589d6cf79e1e1a331f53a31b677cea96ccd2818
limingfang666 <1275012490@qq.com>
2022-08-23 16:26:43 +0800

    测试冲突

--- src/views/detail/comment.vue
+++ src/views/detail/comment.vue
@@ -35,7 +35,7 @@
             <h5></h5>
           </router-link>
           <div class="comment-detail">
-            <p class="p1">这里是评论内容</p>
+            <p class="p1">这里是评论内容--本地修改文件</p>
             <div class="info clearfix">
               <span style="float: left;"></span>
             </div>
