80913da71bd56ff761c679f7d9b41d0e8ce0c36a
limingfang666 <1275012490@qq.com>
2022-08-24 16:14:08 +0800

    实现评论功能

--- src/views/detail/comment.vue
+++ src/views/detail/comment.vue
@@ -1,46 +1,56 @@
 <template>
   <div class="comment-box">
     <h2>关于<span>{{menuInfo.title}}</span>的讨论</h2>
+    <el-form :model="commentForm" :rules="rules" ref="commentForm">
     <div class="comment-text">
       <a href="javascript:;" class="useravatar">
-        <img :src="menuInfo.userInfo && menuInfo.userInfo.avatar">
+        <!-- 这里的头像是当前store中的头像 -->
+        <img :src="$store.state.userInfo && $store.state.userInfo.avatar">
       </a>
-      <div>请先登录后，再评论<router-link :to="{name:'login'}"><em class="login_text">登录</em></router-link></div>
-      
-      <div class="comment-right">
-        <el-input
-          type="textarea"
-          :rows="5"
-          :cols="50"
-          placeholder="请输入内容"
-        >
-        </el-input>
+      <div v-if="!isLogin">请先登录后，再评论<router-link :to="{name:'login'}"><em class="login_text">登录</em></router-link></div>
+      <div v-if="isLogin" class="comment-right">
+        <el-form-item prop="commentText">
+          <el-input
+            v-model="commentForm.commentText"
+            ref="commentTextArea"
+            type="textarea"
+            :rows="5"
+            :cols="50"
+            placeholder="请输入内容"
+          >
+          </el-input>
+        </el-form-item>
         <div class="comment-button" >
           <el-button 
             class="send-comment" 
             type="primary" 
             size="medium"
+            @click="submitComment()"
           >提交</el-button>
         </div>
       </div>
     </div>
+    </el-form>
     <div class="comment-list-box">
-      <ul class="comment-list">
-        <li >
-          <a target="_blank" href="https://i.meishi.cc/cook.php?id=14026963" class="avatar">
-           
-          </a>
-          <router-link to="" class="avatar">
-            <img :src="menuInfo.userInfo && menuInfo.userInfo.avatar">
-            <h5></h5>
+      <ul class="comment-list" v-if="comments.length>0">
+        <li 
+          v-for="comment in comments"
+          :key="comment.commentId"
+        >
+          <router-link :to="{name:'space',query:{userId:comment.userInfo.userId}}" class="avatar">
+            <img :src="comment.userInfo && comment.userInfo.avatar">
+            <h5>{{comment.userInfo.name}}</h5>
           </router-link>
           <div class="comment-detail">
-            <p class="p1">这里是评论内容</p>
+            <p class="p1">{{comment.commentText}}</p>
             <div class="info clearfix">
-              <span style="float: left;"></span>
+              <span style="float: left;">{{comment.updateAt}}</span>
             </div>
           </div>
         </li>
+      </ul>
+      <ul class="comment-list" v-if="comments.length===0">
+        <li><h2>还没有评论内容哦！！！</h2></li>
       </ul>
     </div>
   </div>
@@ -50,12 +60,72 @@ import {getComments,postComment} from '@/service/api';
 export default {
   name: 'Comment',
   props:{
-    menuInfo:{
-    }
+    menuInfo:{}
   },
   data(){
     return {
-      comment:{}
+      comments:[],
+      commentForm:{
+        commentText:''
+      },
+      rules:{
+        commentText:[
+          { required:true, message:"请输入评论内容", trigger: 'blur'}
+        ]
+      }
+    }
+  },
+  computed:{
+    isLogin(){
+      return this.$store.getters.isLogin;
+    }
+  },
+  mounted(){
+    this.getCommentsComm();
+  },
+  methods:{
+    getCommentsComm(){
+      getComments({menuId:this.$route.query.menuId}).then(res=>{
+        if(res.ec===200){
+          this.comments = res.data.comments;
+        }
+      })
+    },
+    submitComment(){
+      this.$refs['commentForm'].validate((valid)=>{
+        if(valid){
+          console.log(this.$refs['commentTextArea'].value);
+          console.log(this.menuInfo.userId);
+          console.log(this.$store.state.userInfo);
+          console.log(this.menuInfo.menuId);
+          // userId为当前菜单所有者 userInfo为当前空间所有者
+          postComment({
+            userId:this.menuInfo.userId,
+            userInfo:this.$store.state.userInfo,
+            menuId:this.menuInfo.menuId,
+            commentText:this.$refs['commentTextArea'].value
+          }).then(res=>{
+            // 成功后重新获取评论
+            this.getCommentsComm();
+
+            this.$message({
+              message:"评论成功！",
+              type:'success'
+            });
+            // 评论成功后，清空textarea框
+            // 使用this.$refs['commentTextArea'].value = ''清空会报错，因为这里使用了双向绑定，每当父组件重新渲染时，该值将被覆盖
+            this.$refs['commentForm'].resetFields();
+          }).catch(err=>{
+            this.$message({
+              message:"评论失败！",
+              type:'error'
+            });
+            this.$nextTick(()=>{
+              this.$refs['commentText'].blur();
+            });
+          });
+        }
+      });
     }
   }
 }
